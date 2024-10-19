fd0cd53bbfdb3937b0558a196e74b1a17f91d9bb
AchalVishnoi <vishnoiachal174@gmail.com>
2024-10-19 16:36:34 +0530

    white pieces added

--- render/main.js
+++ render/main.js
@@ -40,15 +40,40 @@ function initGameRender(data){
       const squareDiv =document.createElement("div");
       squareDiv.classList.add(square.color,"square");
       squareDiv.id=square.id;                            //id passed to each square
+
+   /*filling black pieces*/   
       if(square.id[1]==7) {
          square.piece=pieces.blackPawn(square.id);           //7th row for black pawn
       
       }   
-      if(square.id[1]==2) {
-        square.piece= pieces.whitePawn(square.id);          //2nd row for white pawn
+      if(square.id=="c8"||square.id=="f8") {
+         square.piece=pieces.blackBishop(square.id);           //7th row for black pawn
+      
       }   
-                                                 //a7 b7 .......
-                                                 
+      if(square.id=="b8"||square.id=="g8") {
+         square.piece=pieces.blackKnight(square.id);           //7th row for black pawn
+      
+      }   
+      if(square.id=="a8"||square.id=="h8") {
+         square.piece=pieces.blackRook(square.id);           //7th row for black pawn
+      
+      }   
+      if(square.id=="d8") {
+         square.piece=pieces.blackQueen(square.id);           //7th row for black pawn
+      
+      }   
+      if(square.id=="e8") {
+         square.piece=pieces.blackKing(square.id);           //7th row for black pawn
+      
+      }   
+      
+      //filling white pices//
+
+      if(square.id[1]==2) {
+        square.piece= pieces.whitePawn(square.id);          //2nd row for white pawn a7 b7 .....
+      }
+      
+      
 rowE1.appendChild(squareDiv);
 });
      
